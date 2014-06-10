e1b8a5915bbc012624aa20f1fa42e5c4e54356d5
Olawale <walexi4great@gmail.com>
2014-06-11 00:55:46 +0100

    initial commit for the binary search algorithm implementation

--- BinarySearchAlgorithm/src/binarysearchalgorithm/BinarySearchAlgorithm.java
+++ BinarySearchAlgorithm/src/binarysearchalgorithm/BinarySearchAlgorithm.java
@@ -19,14 +19,14 @@ public class BinarySearchAlgorithm {
      */
     public static void main(String[] args) {
         System.out.println("Enter the value you wanna search for, number must be from 1 to 2000 inclusve");
-        Scanner keyInput = new Scanner(System.in);
-        int key = keyInput.nextInt();
+                Scanner keyInput = new Scanner(System.in);
+                int key = keyInput.nextInt();
         System.out.println("Enter the limit of the arraylist you wanna auto-generate");
-        Scanner listLimitInput = new Scanner(System.in);
-        int listLimit = listLimitInput.nextInt();
+                Scanner listLimitInput = new Scanner(System.in);
+                int listLimit = listLimitInput.nextInt();
         System.out.println("Enter the limit of the array you wanna auto-generate");
-        Scanner arrayLimitInput = new Scanner(System.in);
-        int arrayLimit = arrayLimitInput.nextInt();
+                Scanner arrayLimitInput = new Scanner(System.in);
+                int arrayLimit = arrayLimitInput.nextInt();
         System.out.println("You should find the index of the value you searched"
                 + " for below this line both in the array and arrayList." + "\n" + 
                 "Never mind, jsut playing with arraylist and array." + "\n" + "The "
@@ -34,50 +34,51 @@ public class BinarySearchAlgorithm {
                 + " how to do recursive binary search with both array and arraylist");
         
        System.out.print(binarySearchArrayList(fillArrayList(listLimit), key, 0, fillArrayList(listLimit).size()) + "\n");
+       
        System.out.print(binarySearchArray(fillArray(arrayLimit), key, 0, fillArray(arrayLimit).length) + "\n");
         
  }
    
     
-public static int binarySearchArrayList(ArrayList<Integer> intArray, int key, int low, int high){
-//I didnt bother checking if the array is empty 
-      int mid = (low+high) /2;
-      if (key<intArray.get(mid)){
-      return binarySearchArrayList(intArray, key, low, mid-1); // search for the value in the lower half of the array
-      } else if(key>intArray.get(mid)){ 
-      return binarySearchArrayList(intArray, key, mid+1, high);   // search for the value in the upper half of the array
-      } else  
-    return (int)mid;      //value has been found, return the index
-      }
+    public static int binarySearchArrayList(ArrayList<Integer> intArray, int key, int low, int high){
+            //I didnt bother checking if the array is empty 
+            int mid = (low+high) /2;
+                    if (key<intArray.get(mid)){
+                        return binarySearchArrayList(intArray, key, low, mid-1); // search for the value in the lower half of the array
+                    } else if(key>intArray.get(mid)){ 
+                        return binarySearchArrayList(intArray, key, mid+1, high);   // search for the value in the upper half of the array
+                    } else  
+                        return (int)mid;      //value has been found, return the index
+    }
       
-public static int binarySearchArray(int[] arraytoSearch, int key, int low, int high){
+    public static int binarySearchArray(int[] arraytoSearch, int key, int low, int high){
     
     //compute the middle index
-    int mid = (low + high)/2;
-    if(key<arraytoSearch[mid]){
-        return binarySearchArray(arraytoSearch, key, low, mid -1);// search for the value in the lower half of the array
-    } else if (key > arraytoSearch[mid]){
-        return binarySearchArray(arraytoSearch, key, mid+1, high); // search for the value in the upper half of the array
-    } else 
-        return mid; //  the index of the key value i.e the index of the value you searched for
+            int mid = (low + high)/2;
+                    if(key<arraytoSearch[mid]){
+                        return binarySearchArray(arraytoSearch, key, low, mid -1);// search for the value in the lower half of the array
+                    } else if (key > arraytoSearch[mid]){
+                        return binarySearchArray(arraytoSearch, key, mid+1, high); // search for the value in the upper half of the array
+                    } else 
+                        return mid; //  the index of the key value i.e the index of the value you searched for
 
-}
-
-public static int[] fillArray(int arrayLimit){
-    
-    int [] arraytoFill = new int[arrayLimit];
-    for (int i = 0; i < arrayLimit; i++) {
-        arraytoFill[i] = i + 1; //adding 1 to exclude 0
     }
-return arraytoFill;
-}
 
-public static ArrayList<Integer> fillArrayList(int listSize){
-    ArrayList<Integer> arrayTobesoted= new ArrayList();
-        for (int i = 0; i < 2000; i++) {
-            arrayTobesoted.add(i+1);
-        }
-return arrayTobesoted;
-}
+    public static int[] fillArray(int arrayLimit){
+    
+            int [] arraytoFill = new int[arrayLimit];
+                for (int i = 0; i < arrayLimit; i++) {
+                        arraytoFill[i] = i + 1; //adding 1 to exclude 0
+                }
+        return arraytoFill;
+    }
+
+    public static ArrayList<Integer> fillArrayList(int listSize){
+            ArrayList<Integer> arrayTobesoted= new ArrayList();
+                for (int i = 0; i < 2000; i++) {
+                    arrayTobesoted.add(i+1);
+                }
+        return arrayTobesoted;
+    }
 
 }
